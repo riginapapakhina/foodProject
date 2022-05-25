02b38b124411bf74fd47d689792ba293c407c1bc
Rigina <papakhina.rigina@yandex.ru>
2022-05-25 10:38:56 +0300

    modal window modified

--- js/script.js
+++ js/script.js
@@ -101,19 +101,20 @@ tabsParent.addEventListener('click', function(event) {
       modalCloseBtn = document.querySelector('[data-close]');
 
   modalTrigger.forEach(btn => {
-      btn.addEventListener('click', function() {
-          modal.classList.add('show');
-          modal.classList.remove('hide');
-          // Либо вариант с toggle - но тогда назначить класс в верстке
-          document.body.style.overflow = 'hidden';
-      });
+      btn.addEventListener('click', openModal);
   });
 
   function closeModal() {
       modal.classList.add('hide');
       modal.classList.remove('show');
-      // Либо вариант с toggle - но тогда назначить класс в верстке
       document.body.style.overflow = '';
+  }
+
+  function openModal() {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+      clearInterval(modalTimerId);
   }
   
   modalCloseBtn.addEventListener('click', closeModal);
@@ -129,4 +130,14 @@ tabsParent.addEventListener('click', function(event) {
           closeModal();
       }
   });
+
+  const modalTimerId = setTimeout(openModal, 3000);
+
+  function showModalByScroll() {
+      if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+          openModal();
+          window.removeEventListener('scroll', showModalByScroll);
+      }
+  }
+  window.addEventListener('scroll', showModalByScroll);
 });
