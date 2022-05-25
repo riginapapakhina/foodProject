ac6681ced48ce1fbb8bf5066f314a107c4facbce
Rigina <papakhina.rigina@yandex.ru>
2022-05-25 10:36:17 +0300

    modal window creation

--- js/script.js
+++ js/script.js
@@ -94,4 +94,39 @@ tabsParent.addEventListener('click', function(event) {
 
   setClock('.timer', deadline);
 
+  // Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalCloseBtn = document.querySelector('[data-close]');
+
+  modalTrigger.forEach(btn => {
+      btn.addEventListener('click', function() {
+          modal.classList.add('show');
+          modal.classList.remove('hide');
+          // Либо вариант с toggle - но тогда назначить класс в верстке
+          document.body.style.overflow = 'hidden';
+      });
+  });
+
+  function closeModal() {
+      modal.classList.add('hide');
+      modal.classList.remove('show');
+      // Либо вариант с toggle - но тогда назначить класс в верстке
+      document.body.style.overflow = '';
+  }
+  
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+          closeModal();
+      }
+  });
+
+  document.addEventListener('keydown', (e) => {
+      if (e.code === "Escape" && modal.classList.contains('show')) { 
+          closeModal();
+      }
+  });
 });
