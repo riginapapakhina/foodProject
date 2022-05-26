a2168c5d61d5364345cefb7b34a7103895151a6c
Rigina <papakhina.rigina@yandex.ru>
2022-05-26 14:20:31 +0300

    fetch API

--- js/script.js
+++ js/script.js
@@ -234,29 +234,36 @@ tabsParent.addEventListener('click', function(event) {
           `;
           form.insertAdjacentElement('afterend', statusMessage);
       
-          const request = new XMLHttpRequest();
-          request.open('POST', 'server.php');
-          request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+     
+          
           const formData = new FormData(form);
 
           const object = {};
           formData.forEach(function(value, key){
               object[key] = value;
           });
-          const json = JSON.stringify(object);
+      
 
-          request.send(json);
+          fetch('server.php', {
+            method: "POST",
+            headers: {
+              'Content-type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify(object)
+          }).then(data => data.text())
+          .then(data => {
+            console.log(data);
+            showThanksModal(message.success);
+            
+            statusMessage.remove();
+          }).catch(()=> {
+            showThanksModal(message.failure)
+          }).finally(()=> {
+            form.reset();
+          })
 
-          request.addEventListener('load', () => {
-              if (request.status === 200) {
-                  console.log(request.response);
-                  showThanksModal(message.success);
-                  statusMessage.remove();
-                  form.reset();
-              } else {
-                  showThanksModal(message.failure);
-              }
-          });
+
+
       });
   }
 
@@ -284,4 +291,5 @@ tabsParent.addEventListener('click', function(event) {
           closeModal();
       }, 4000);
   }
+  
 });
