cb179c03dcdb2f895bd8bdabe49230f010a15cef
Rigina <papakhina.rigina@yandex.ru>
2022-05-27 17:01:53 +0300

    slider added

--- js/script.js
+++ js/script.js
@@ -191,9 +191,16 @@ tabsParent.addEventListener('click', function(event) {
     return await res.json();
   };
 
-  getResource(' http://localhost:3000/menu')
+//   getResource('http://localhost:3000/menu')
+// .then(data =>{
+  // data.forEach(({img, altimg, title, descr, price}) =>{
+  //   new MenuCard(img, altimg, title, descr, price, '.menu .container').render
+  // });
+// });
+
+axios.get('http://localhost:3000/menu')
 .then(data =>{
-  data.forEach(({img, altimg, title, descr, price}) =>{
+  data.data.forEach(({img, altimg, title, descr, price}) =>{
     new MenuCard(img, altimg, title, descr, price, '.menu .container').render
   });
 });
@@ -274,7 +281,50 @@ const postData= async (url, data)=>{
           closeModal();
       }, 4000);
   }
-  fetch('db.json')
-    .then(data => data.json())
-    .then(res => console.log(res));
+
+
+    //Slider
+    const slides=document.querySelectorAll('.offer__slide'),
+    prev=document.querySelector('.offer__slider-prev'),
+    next=document.querySelector('.offer__slider-next'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current');
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+        
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    function plusSlides (n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
 });
