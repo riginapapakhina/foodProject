9742b20e054e6b0e754b8c4617978b6885d657c0
Rigina <papakhina.rigina@yandex.ru>
2022-05-27 16:18:30 +0300

    receive data

--- js/script.js
+++ js/script.js
@@ -182,32 +182,21 @@ tabsParent.addEventListener('click', function(event) {
       }
   }
 
-  new MenuCard(
-      "img/tabs/vegy.jpg",
-      "vegy",
-      'Меню "Фитнес"',
-      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-      9,
-      ".menu .container"
-  ).render();
 
-  new MenuCard(
-      "img/tabs/post.jpg",
-      "post",
-      'Меню "Постное"',
-      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-      14,
-      ".menu .container"
-  ).render();
+  const getResource= async (url, data)=>{
+    const res = await fetch(url);
+    if(!res.ok){
+     throw new Error(`Could not fetch ${url},status: ${res.status}`)
+    }
+    return await res.json();
+  };
 
-  new MenuCard(
-      "img/tabs/elite.jpg",
-      "elite",
-      'Меню “Премиум”',
-      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-      21,
-      ".menu .container"
-  ).render();
+  getResource(' http://localhost:3000/menu')
+.then(data =>{
+  data.forEach(({img, altimg, title, descr, price}) =>{
+    new MenuCard(img, altimg, title, descr, price, '.menu .container').render
+  });
+});
 
   // Forms
 
@@ -219,10 +208,21 @@ tabsParent.addEventListener('click', function(event) {
   };
 
   forms.forEach(item => {
-      postData(item);
+      bindPostData(item);
   });
 
-  function postData(form) {
+const postData= async (url, data)=>{
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: data
+  });
+  return await res.json();
+};
+
+  function bindPostData(form) {
       form.addEventListener('submit', (e) => {
           e.preventDefault();
 
@@ -236,18 +236,11 @@ tabsParent.addEventListener('click', function(event) {
       
           const formData = new FormData(form);
 
-          const object = {};
-          formData.forEach(function(value, key){
-              object[key] = value;
-          });
-
-          fetch('server.php', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(object)
-          }).then(data => {
+          const json= JSON.stringify(Object.fromEntries(formData.entries()));
+        
+        
+          postData('http://localhost:3000/requests', json)
+          .then(data => {
               console.log(data);
               showThanksModal(message.success);
               statusMessage.remove();
