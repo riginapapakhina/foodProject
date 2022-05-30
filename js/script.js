c133f48d0e836ea72c149887207a76e55ff47dd3
Rigina <papakhina.rigina@yandex.ru>
2022-05-30 15:27:20 +0300

    regexp

--- js/script.js
+++ js/script.js
@@ -361,10 +361,10 @@ const postData= async (url, data)=>{
     }
 
     next.addEventListener('click', () => {
-        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+        if (offset == (+width.replace(/\D/g, '') * (slides.length - 1))) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2); 
+            offset += +width.replace(/\D/g, ''); 
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -387,9 +387,9 @@ const postData= async (url, data)=>{
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = +width.replace(/\D/g, '') * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= +width.replace(/\D/g, '');
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -415,7 +415,7 @@ const postData= async (url, data)=>{
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = +width.replace(/\D/g, '') * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
