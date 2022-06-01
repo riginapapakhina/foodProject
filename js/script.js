27a00481ca58258298a0d11b6c33d2379d92e76b
Rigina <papakhina.rigina@yandex.ru>
2022-06-01 16:23:59 +0300

    babel and polyfills added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
